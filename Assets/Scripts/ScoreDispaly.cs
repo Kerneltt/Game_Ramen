6cd8ced71e7dcb4e50356673366bb3b8d8a146b3
Kerneltt <damiankerneltt@gmail.com>
2018-11-12 00:17:19 -0600

    Score Manager Save

---
+++ Assets/Scripts/ScoreDispaly.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDispaly : MonoBehaviour {
+    [SerializeField]
+    GameObject ScoreParent;
+    [SerializeField]
+    GameObject scorePrefab;
+    string[] scorestring;
+	// Use this for initialization
+	void Start () {
+        UpdateScores();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpdateScores()
+    {        
+        foreach (Transform children in ScoreParent.GetComponentInChildren<Transform>())
+        {
+            Destroy(children.gameObject);
+        }
+        scorestring = PlayerPrefs.GetString("ScoreLogs").Split('*');
+        foreach (string registry in scorestring)
+        {
+            string[] substring = registry.Split('|');
+            if (registry!="")
+            {
+                GameObject temp;
+                temp = Instantiate(scorePrefab);
+                temp.GetComponentInChildren<Button>().gameObject.GetComponentInChildren<Text>().text = substring[0]+ "\n"+ substring[1];
+                temp.transform.SetParent(ScoreParent.transform);
+            }            
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreSave.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSave : MonoBehaviour {
+    public GameObject[] players;
+    public InputField name;
+    string score;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnSavePressed()
+    {
+        if (players[0].activeSelf==true)
+        {
+            score = "";
+            score += name.text + "|" + System.DateTime.Now.ToString();
+            foreach (GameObject player in players)
+            {
+                if (player.activeSelf == true)
+                {
+                    score += "#" + player.GetComponent<InputField>().text + ": " + player.GetComponent<PointPlayer>().points[8].GetComponent<InputField>().text + "*";
+                }
+            }
+            PlayerPrefs.SetString("ScoreLogs", PlayerPrefs.GetString("ScoreLogs") + score);
+            print(PlayerPrefs.GetString("ScoreLogs"));
+        }        
+    }
+}
