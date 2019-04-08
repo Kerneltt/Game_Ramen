41bd61df70dad2a092c8be915b36e65e9a7fe1df
Rod <rodrigo.ramirez.sc@gmail.com>
2019-04-08 04:18:43 -0600

    3D dice for UI

---
+++ Assets/Scripts/DiceRoller2/ColorMyDice.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorMyDice : MonoBehaviour
+{
+    [SerializeField]
+    DiceManager2 diceManager;
+    [SerializeField]
+    Material diceMat;
+    [SerializeField]
+    Color currentColor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentColor = diceManager.diceColor;
+        diceMat.color = currentColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentColor = diceManager.diceColor;
+        ColorMe();
+    }
+
+    public void ColorMe()
+    {
+        diceMat.color = currentColor;
+    }
+}
