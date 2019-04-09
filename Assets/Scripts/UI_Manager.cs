1fb3f0f46a9cc6ecbb962e7d37ba9a765aad41c3
Rod <rodrigo.ramirez.sc@gmail.com>
2019-04-09 01:55:38 -0600

    TrashBin update

--- Assets/Scripts/UI_Manager.cs
+++ Assets/Scripts/UI_Manager.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class UI_Manager : MonoBehaviour
 {
     [SerializeField]
-    CanvasScaler canvasScaler;
+    VerticalLayoutGroup vertLayout;
     [SerializeField]
-    RectTransform fitRectTransform;
+    int leftPadding_iphoneX;
     [SerializeField]
+    int leftPadding_16_9;
+    [SerializeField]
+    int screenHeight;
+    [SerializeField]
+    int currentLeftPadding;
+    /*[SerializeField]
     Vector2 refRes_iPhoneX = new Vector2(360.0f, 780.0f);
     [SerializeField]
-    Vector2 Aspect_16_9_iPhone = new Vector2(360.0f, 640.0f);
+    Vector2 Aspect_16_9_iPhone = new Vector2(360.0f, 640.0f);*/
 
     //iPhoneX height variants
     private int iPhoneX_Height = 2436;
@@ -23,49 +30,26 @@ public class UI_Manager : MonoBehaviour
     private int iPhone5_Height = 1136;
     private int iPhone6_Height = 1334;
 
-    private void Awake()
-    {
-
-    }
-
     private void Start()
     {
+        currentLeftPadding = vertLayout.padding.left;
+        screenHeight = Screen.height;
+
+        if (screenHeight == iPhoneX_Height || screenHeight == iPhoneXR_Height || screenHeight == iPhoneXSMAX_Height)
+        {
+            vertLayout.padding.left = 20;
+        }
+        else
+        {
+            vertLayout.padding.left = 20;
+            vertLayout.padding.right = 5;
+        }
         Canvas.ForceUpdateCanvases();
 
-        if (Screen.height == iPhoneX_Height || Screen.height == iPhoneXR_Height || Screen.height == iPhoneXSMAX_Height)
-        {
-            //Set canvas scaler reference resolution
-            canvasScaler.referenceResolution = refRes_iPhoneX;
-
-            //Set Fit Content Rect Transform Values
-            fitRectTransform.anchorMin = new Vector2(0.5f, 0.5f);
-            fitRectTransform.anchorMax = new Vector2(0.5f, 0.5f);
-            //fitRectTransform.sizeDelta = refRes_iPhoneX;
-            fitRectTransform.rect.Set(0, 0, refRes_iPhoneX.x, refRes_iPhoneX.y);
-            Debug.Log("Fit Content RectTransform = " + fitRectTransform.rect);
-
-        }
-        else if (Screen.height == iPhone5_Height || Screen.height == iPhone6_Height)
-        {
-            //Set canvas scaler reference resolution
-            canvasScaler.referenceResolution = Aspect_16_9_iPhone;
-
-            //Set Fit Content Rect Transform Values
-            fitRectTransform.anchorMin = new Vector2(0.5f, 0.5f);
-            fitRectTransform.anchorMax = new Vector2(0.5f, 0.5f);
-            fitRectTransform.sizeDelta = Aspect_16_9_iPhone;
-            fitRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 0);
-            fitRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 0);
-        }
     }
 
     // Update is called once per frame
     void Update()
-    {
-
-    }
-
-    private void LateUpdate()
     {
 
     }
