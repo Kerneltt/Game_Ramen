10fbcfcd99567b86a80f210545f31d73af21fec4
Rod <rodrigo.ramirez.sc@gmail.com>
2019-04-11 16:22:39 -0600

    Survey button implemented

---
+++ Assets/Scripts/DiceRoller2/OpenSurvey.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebInterface : MonoBehaviour
+{
+    public void OpenWebsite(string url)
+    {
+            Application.OpenURL(url);
+    }
+}
